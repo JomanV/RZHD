012c52b44833b20a693cbef679ff53c085217156
Joman_V <German.Vlasenko@bk.ru>
2022-05-16 21:24:15 +0300

    add functions (create_table, fill, check_tables, db_init) and start code

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print('hello, world!')
+#TODO1: add all tables, one commit per adding one table
+#TODO2: fill all tables with separate functions, one commit per filling one table with a function; perform a check after each filling with a "select * below"
+#TODO3: make a select query that shows run timetables like in http://izformatika.ru/mod/assign/view.php?id=2564
+#TODO4: (optional for this task) add foreign keys
+
+import sqlite3
+
+create_table=[]
+tables_fill=[]
+
+
+def _create_tables():
+    conn = sqlite3.connect('railways.db')
+    for i in range(len(create_table)):
+        conn.execute(create_table[i][0])
+        conn.commit()
+    conn.close()  
+
+def _fill():
+    conn = sqlite3.connect('railways.db')
+    conn.execute()
+    conn.commit()
+    conn.close()     
+
+def _check_tables():
+    conn = sqlite3.connect('railways.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    SELECT name FROM sqlite_master WHERE type='table';''')
+    res = cursor.fetchall()
+    for r in res:
+        print(r)
+    
+def _db_init():    
+    _create_tables()
+    _check_tables()
+     #_fill()
+
+from os.path import exists
+
+if not exists('railways.db'):
+    _db_init()
+
+#fill_locomotive_types()
+conn = sqlite3.connect('railways.db')
+cursor = conn.cursor()
+
+cursor.execute()
+res = cursor.fetchall()
+
+for r in res:
+    print(r)
